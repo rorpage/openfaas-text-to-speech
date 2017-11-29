f74a2e7c72ac1107c6cb93553c1855d41ed629e8
Robbie Page <rob@rorpage.com>
2017-11-28 19:57:56 -0500

    update sample, reorder Dockerfile, delete tmp mp3

--- text-to-speech/script.sh
+++ text-to-speech/script.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 UUID=$(cat /proc/sys/kernel/random/uuid)
-python /usr/local/bin/gtts-cli.py - -l 'en' -o /tmp/$UUID.mp3 && cat /tmp/$UUID.mp3
+python /usr/local/bin/gtts-cli.py - -l 'en' -o /tmp/$UUID.mp3
+cat /tmp/$UUID.mp3
+rm /tmp/$UUID.mp3
